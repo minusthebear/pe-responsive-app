aa27a4330d5ffc83935f4ea4daf02bb7d7c29ae5
minusthebear <matt.hamann1982@gmail.com>
2019-07-17 14:05:01 -0700

    Todays deals

--- src/components/MainBodyContainer.js
+++ src/components/MainBodyContainer.js
@@ -1,12 +1,21 @@
 import React from "react";
 import ProductMarkets from "./ProductMarkets";
 import GetStarted from "./GetStarted";
+import deal from '../images/panel/deal.png';
 
 export default function MainBodyContainer() {
     return (
         <div className="platt-main-body">
             <ProductMarkets />
             <GetStarted/>
+            <div className="recently-viewed-container" >
+                <div className="todays-deals">
+                    <img src={deal} alt="deal" />
+                </div>
+                <div className="recently-viewed">
+                    Recently Viewed
+                </div>
+            </div>
         </div>
     );
 }
